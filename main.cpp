5fa7e46cb3773614f67fc4516eb3d383e56d2b78
jacobkuenning <jacob.kuenning@gmail.com>
2025-11-22 18:38:38 -0800

    input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "script.h"
 #include "sequencer.h"
+#include "message.h"
 #include "rtmidi/RtMidi.h"
 #include "globals.h"
 #include <thread>
@@ -7,6 +8,8 @@
 #include <chrono>
 #include <functional>
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
 
 std::vector<std::thread> threads;
 std::vector<sequencer*> seqs;
@@ -16,6 +19,20 @@ bool done = false;
 std::mutex outputMx;
 std::mutex vectorMx;
 std::mutex midiMx;
+
+void input(){
+    while(!done){
+        int c = getchar();
+        if (c=='q'){
+            vectorMx.lock();
+            for (sequencer* n : seqs){
+                n->running = false;
+            }
+            done = true;
+            vectorMx.unlock();
+        }
+    }
+}
 
 // create new sequencer, start it at line i, it will run until it reaches an @ END
 void branch(int i){
@@ -29,8 +46,40 @@ void branch(int i){
     return;
 }
 
+void setRawMode(bool enable) { 
+    static termios oldt; 
+    termios newt; 
+    if (enable) { 
+        tcgetattr(STDIN_FILENO, &oldt); 
+        newt = oldt; 
+        newt.c_lflag &= ~(ICANON | ECHO); // no buffering, no echo tcsetattr(STDIN_FILENO, TCSANOW, &newt); } else { tcsetattr(STDIN_FILENO, TCSANOW, &oldt); } }
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+    } else { 
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+    } 
+}
+
+void killAllMidi(){
+    std::vector<message> offMessages;
+
+    for (int i = 0; i < 16; i++){
+        for (int n = 0; n < 108; n++)
+        {
+            std::vector<unsigned char> msg(3);
+            msg[0] = i | 0x80;
+            msg[1] = n;
+            msg[2] = 0;
+            midiout->sendMessage(&msg);
+        }
+    }
+}
+
 int main(int argc, char** argv){
     midiout->openPort(0);
+    setRawMode(true);
+
+    std::thread inputThread = std::thread(&input);
+
     s = new script(argv[1]); // create script
     sequencer* seq = new sequencer(s,true,0,branch, midiout);
     seqs.push_back(seq);
@@ -55,9 +104,12 @@ int main(int argc, char** argv){
         std::this_thread::sleep_for(std::chrono::microseconds(100));
     }
 
+    inputThread.join();
+    killAllMidi();
+
     delete s;
 
     //delete midiout;
-
+    setRawMode(false);
     return 0;
 }
